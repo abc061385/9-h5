29df2a55ac6807926fe233c56fe7184bf64b1733
Parker <parker@MacBook-Pro.local>
2025-07-14 16:51:33 +0800

    feat(component): add header-with-back

---
+++ src/components/header-with-back.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import { ReactNode, useMemo } from "react";
+import { useRouter } from "next/navigation";
+
+type IProps = {
+  title?: ReactNode;
+  algin?: "center" | "right";
+};
+export const HeaderWithBack = ({ title, algin = "right" }: IProps) => {
+  const warpClass = cn(["flex items-center"]);
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  const notHistory = useMemo(() => {
+    if (typeof window !== "undefined" && window.history.length > 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }, [router]);
+  return (
+    <div className={warpClass}>
+      <i
+        className={cn(["cursor-pointer", notHistory && "text-red-400"])}
+        onClick={handleBack}
+      >
+        {"←"}
+      </i>
+      <span className="flex-1 flex justify-center">
+        {algin === "center" ? title : null}
+      </span>
+      <span>{algin === "right" ? title : null}</span>
+    </div>
+  );
+};
