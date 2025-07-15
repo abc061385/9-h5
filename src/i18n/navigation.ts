a536f6096fd9f2e945c39b6ec28bde9ca47f9dbb
Parker <parker@MacBook-Pro.local>
2025-07-15 10:37:02 +0800

    add reouterMap

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -5,3 +5,10 @@ import { routing } from "./routing";
 // APIs that consider the routing configuration
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
+
+export const routerMap = {
+  index: "/",
+  home: "/home",
+  login: "/login",
+  register: "/register",
+};
