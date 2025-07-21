02f149e3e9f9655d67b0268db4802f7ebbc520e1
Parker <parker@MacBook-Pro.local>
2025-07-21 15:04:30 +0800

    feat(fund/header): moidfy height

--- src/views/fund/header.tsx
+++ src/views/fund/header.tsx
@@ -6,7 +6,7 @@ const FundHeaderBox = () => {
   const { push } = useRouter();
   const t = useTrans();
   return (
-    <div className="flex items-center justify-between  px-4 size-full">
+    <div className="flex items-center justify-between  px-4 h-11">
       <h1 className="text-lg font-bold">{t("基金")}</h1>
       <Icon
         name="order"
