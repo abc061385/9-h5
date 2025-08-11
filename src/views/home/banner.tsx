03687ad2cce005b02f01973d6e02bc92d2f5bfc5
Will <Will.com>
2025-08-11 22:02:36 +0800

    feat: import language

--- src/views/home/banner.tsx
+++ src/views/home/banner.tsx
@@ -48,7 +48,7 @@ const BannerBox = () => {
       className="h-[174px]"
     >
       <SwiperSlide>
-        <div className="flex h-full items-center justify-between bg-[url('/images/home/home_banner.png')] bg-contain bg-no-repeat p-content !py-7">
+        <div className="flex h-full justify-between bg-[url('/images/home/home_banner.png')] bg-contain bg-no-repeat p-content !py-7">
           <div>
             <h2 className="text-xl leading-6 mb-2.5 font-bold">{t("公司")}</h2>
             <p className="text-sm leading-3.5 w-[52%]">{t("公司简介")}</p>
