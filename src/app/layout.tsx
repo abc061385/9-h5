b4b8dd073f34ac559f6054fe4aa81ac7d784e79c
Will <Will.com>
2025-08-12 19:20:04 +0800

    feat: del ai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import Script from "next/script";
 import { PropsWithChildren } from "react";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <Script
-        src="https://plugin-code.salesmartly.com/js/project_315109_323721_1745570348.js"
-        strategy="afterInteractive"
-      />
       <>{children}</>
     </>
   );
