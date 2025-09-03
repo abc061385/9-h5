f8d9ab8b325efc291f759e945947cba67fc48d1e
Will <Will.com>
2025-09-03 19:23:25 +0800

    feat: studio pages

---
+++ src/app/[locale]/studio/page.tsx
@@ -0,0 +1,25 @@
+import useInitLocale from "@/hooks/useInitLocale";
+import getStaticParams from "@/lib/getStaticParams";
+import StudioView from "@/views/studio/index";
+import { Metadata } from "next";
+import { generateSEO } from "@/lib/seo";
+
+export async function generateMetadata({
+  params,
+}: RootProps): Promise<Metadata> {
+  const { locale } = await params;
+  return generateSEO({
+    title: "工作室",
+    locale,
+  });
+}
+
+export default function Studio({ params }: RootProps) {
+  useInitLocale(params);
+
+  return <StudioView />;
+}
+
+export function generateStaticParams() {
+  return getStaticParams();
+}
---
+++ src/views/studio/index.tsx
@@ -0,0 +1,339 @@
+"use client";
+
+import BaseImage from "@/components/base-image";
+import { HeaderWithBack } from "@/components/header-with-back";
+import ViewLayout from "@/components/layout";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z, { useRootReg } from "@/lib/z";
+import { TextError } from "@/components/input/text-error";
+import { useTrans } from "@/hooks/useTrans";
+import { Icon } from "@/components/icon";
+import { useRef, useState } from "react";
+import { cn } from "@/lib/utils";
+
+type FormData = {
+  googleCode: string;
+  email: string;
+  emailCode: string;
+  oldEmailCode: string;
+};
+
+const StudioView = () => {
+  const t = useTrans();
+  const reg = useRootReg();
+  const imageRefs = useRef<(HTMLInputElement | null)[]>([]);
+  const videoRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const [needLecturer, setNeedLecturer] = useState(0);
+  const [isAgreement, setIsAgreement] = useState(false);
+
+  const Schema = z.object({
+    googleCode: reg.googleVerifyCode,
+    email: reg.email,
+    emailCode: reg.googleVerifyCode,
+    oldEmailCode: z.any().nullable(),
+  });
+
+  const {
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(Schema),
+    mode: "onChange",
+    reValidateMode: "onChange",
+  });
+
+  return (
+    <ViewLayout
+      heightFull
+      header={
+        <HeaderWithBack
+          title={
+            <div className="flex-1 flex justify-center items-center relative">
+              <span></span>
+              Studio Application
+              <span
+                onClick={() => alert(1)}
+                className="absolute right-[-24px] font-medium text-base text-primary"
+              >
+                Records
+              </span>
+            </div>
+          }
+          algin="center"
+        />
+      }
+    >
+      <BaseImage src="/images/studio/banner.png" className="w-full h-[148px]" />
+      <div className="p-content">
+        <h2 className="font-bold text-xl leading-5 mb-2">Basic Information</h2>
+        <form className="grow" autoComplete="off">
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Phone number
+            </legend>
+            <label className="input w-full h-12">
+              <div className="h-6 border-r border-border2 pr-2.5">
+                <b>+852</b>
+                <Icon
+                  name="right-enter"
+                  className="w-1.5 h-2.5 rotate-90 ml-3"
+                />
+              </div>
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter your phone number"
+                className="grow placeholder:text-sm"
+              />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Email
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="email"
+                {...register("email")}
+                placeholder={t("请输入要绑定的邮箱账号")}
+                className="grow placeholder:text-sm"
+              />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend flex-col items-start gap-1">
+              <h3 className="font-medium text-base">Venue Address</h3>
+              <p className="text-sm text-text4 font-normal">
+                Please provide as detailed an address as possible (country,
+                city, and specific address, including venue name, street number,
+                and street name) for subsequent verification.
+              </p>
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter the venue address"
+                className="grow placeholder:text-sm"
+              />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Contact
+            </legend>
+            <label className="input w-full h-12">
+              <div className="h-6 border-r border-border2 pr-2.5">
+                <b>TG</b>
+                <Icon
+                  name="right-enter"
+                  className="w-1.5 h-2.5 rotate-90 ml-3"
+                />
+              </div>
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter contact information"
+                className="grow placeholder:text-sm"
+              />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Venue Type
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please select venue type"
+                className="grow placeholder:text-sm"
+              />
+              <Icon name="right-enter" className="w-1.5 h-2.5 rotate-90 ml-3" />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+
+          <h3 className="text-lg font-bold mt-11 mb-6">
+            Venue rental certificate
+          </h3>
+          <div className="grid grid-cols-2 gap-2">
+            {[...new Array(2)].map((_, i) => {
+              return (
+                <div key={i}>
+                  <div
+                    className="h-50 bg-bg3 rounded-lg flex flex-col gap-4 items-center justify-center cursor-pointer"
+                    onClick={() => imageRefs.current[i]?.click()}
+                  >
+                    <Icon name="add" className="size-6" />
+                    <span className="text-text4 text-sm">
+                      Click to upload photos
+                    </span>
+                  </div>
+                  <input
+                    type="file"
+                    accept="image/png,image/jpeg"
+                    ref={(el) => {
+                      imageRefs.current[i] = el;
+                    }}
+                    style={{ display: "none" }}
+                    onChange={(e) => console.log(e)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+
+          <h3 className="text-lg font-bold mt-11">Live video</h3>
+          <p className="text-text4 text-sm my-4">
+            Please upload a live video. Each video should be ≥ 10 minutes long
+            and have more than 20 participants. The video should clearly show
+            the scene and the teaching situation.
+          </p>
+          <div className="grid grid-cols-2 gap-2">
+            {[...new Array(2)].map((_, i) => {
+              return (
+                <div key={i}>
+                  <div
+                    className="h-50 bg-bg3 rounded-lg flex flex-col gap-4 items-center justify-center cursor-pointer"
+                    onClick={() => videoRefs.current[i]?.click()}
+                  >
+                    <Icon name="add" className="size-6" />
+                    <span className="text-text4 text-sm">
+                      Click to upload videos
+                    </span>
+                  </div>
+                  <input
+                    type="file"
+                    accept="video/mp4"
+                    ref={(el) => {
+                      videoRefs.current[i] = el;
+                    }}
+                    style={{ display: "none" }}
+                    onChange={(e) => console.log(e)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Operational Plan
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter your operating plan"
+                className="grow placeholder:text-sm"
+              />
+              <Icon name="right-enter" className="w-1.5 h-2.5 rotate-90 ml-3" />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Number of participants
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter the number of participants"
+                className="grow placeholder:text-sm"
+              />
+              <Icon name="right-enter" className="w-1.5 h-2.5 rotate-90 ml-3" />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+
+          <div className="flex items-center justify-between mt-4 mb-2">
+            <h4 className="font-medium text-sm">Do you need a lecturer?</h4>
+            <div className="flex items-center gap-6">
+              {["YES", "NO"].map((v, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="flex gap-2"
+                    onClick={() => setNeedLecturer(i)}
+                  >
+                    <div
+                      className={cn(
+                        "border-2 rounded-full size-6 flex items-center justify-center",
+                        needLecturer === i
+                          ? "!bg-primary !border-primary"
+                          : " !border-border1 !bg-transparent"
+                      )}
+                    >
+                      <Icon name="duigou" />
+                    </div>
+                    <span className="font-medium">{v}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Language of Instruction
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter the language of instruction"
+                className="grow placeholder:text-sm"
+              />
+              <Icon name="right-enter" className="w-1.5 h-2.5 rotate-90 ml-3" />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend font-medium text-sm py-3.5">
+              Receiving address
+            </legend>
+            <label className="input w-full h-12">
+              <input
+                type="text"
+                {...register("email")}
+                placeholder="Please enter the  address"
+                className="grow placeholder:text-sm"
+              />
+              <Icon name="right-enter" className="w-1.5 h-2.5 rotate-90 ml-3" />
+            </label>
+            <TextError>{errors?.email?.message}</TextError>
+          </fieldset>
+        </form>
+        <div className="flex items-center text-xs text-text4 mt-8">
+          <label className="label">
+            <input
+              type="checkbox"
+              className="checkbox checkbox-neutral"
+              checked={isAgreement}
+              onChange={(e) => setIsAgreement(e.target.checked)}
+            />
+
+            {t("我已阅读")}
+          </label>
+          <span className="text-text1">
+            《 Training Center/Studio Program Rules 》
+          </span>
+        </div>
+        <button className="btn btn-primary w-full mt-4">
+          Submit your application
+        </button>
+      </div>
+    </ViewLayout>
+  );
+};
+
+export default StudioView;
