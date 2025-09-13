3d18b71410cd266c35cc4336515f703718a24fca
Will <Will.com>
2025-09-13 10:29:32 +0800

    feat: add loading

--- src/views/activity/index.tsx
+++ src/views/activity/index.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { useLocale } from "next-intl";
 import { useLocationHref } from "@/hooks/useLocationHref";
 import { useTrans } from "@/hooks/useTrans";
+import { ShowIf } from "@/components/show-if";
 
 const ActivityView = () => {
   const locale = useLocale();
@@ -15,17 +16,21 @@ const ActivityView = () => {
   const { goToActivity } = useLocationHref();
 
   const [activityList, setActivityList] = useState<ActivityList[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const getActivityList = useCallback(async () => {
+    setLoading(true);
     try {
       const res = await api.userActivity.getActivityListUsingGet({
         pageNo: 1,
         pageSize: 100,
       });
       if (res.code === 200) {
+        setLoading(false);
         setActivityList(res.data?.list || []);
       }
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   }, []);
@@ -36,43 +41,51 @@ const ActivityView = () => {
 
   return (
     <ViewLayout
+      heightFull
       header={<HeaderWithBack title={t("activityCenter")} algin="center" />}
     >
       <div className="p-content">
-        {activityList.map((v) => {
-          return (
-            <div
-              key={v.id}
-              className="py-6 border-b border-border2 last:border-0"
-              onClick={() => goToActivity(v.id.toString())}
-            >
-              <Image
-                src={
-                  ["zh-Hans", "zh-Hant"].indexOf(locale) !== -1
-                    ? v.bannerZh
-                    : v.bannerEn
-                }
-                alt=""
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                  position: "relative",
-                }}
-                className="rounded-xl"
-              />
+        {
+          <ShowIf
+            condition={!loading}
+            elseEl={<span className="loading flex mx-auto mt-10"></span>}
+          >
+            {activityList.map((v) => {
+              return (
+                <div
+                  key={v.id}
+                  className="py-6 border-b border-border2 last:border-0"
+                  onClick={() => goToActivity(v.id.toString())}
+                >
+                  <Image
+                    src={
+                      ["zh-Hans", "zh-Hant"].indexOf(locale) !== -1
+                        ? v.bannerZh
+                        : v.bannerEn
+                    }
+                    alt=""
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                      position: "relative",
+                    }}
+                    className="rounded-xl"
+                  />
 
-              <div
-                className="whitespace-pre-wrap mt-4 line-clamp-3"
-                dangerouslySetInnerHTML={{
-                  __html: v.content ? JSON.parse(v.content)[locale] : "",
-                }}
-              ></div>
-            </div>
-          );
-        })}
+                  <div
+                    className="whitespace-pre-wrap mt-4 line-clamp-3"
+                    dangerouslySetInnerHTML={{
+                      __html: v.content ? JSON.parse(v.content)[locale] : "",
+                    }}
+                  ></div>
+                </div>
+              );
+            })}
+          </ShowIf>
+        }
       </div>
     </ViewLayout>
   );
