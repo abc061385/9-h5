6e8e2db38ca991b3958ff7ffbbb7e1185aeda67b
abc <abc@example.com>
2025-09-26 20:39:24 +0800

    modify type

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -46,7 +46,7 @@ export const typeMap: { [key: string]: string } = {
   CLAIM_DYNAMIC_REWARDS_SMART: "depositIntoSmartWallet",
   CLAIM_DYNAMIC_REWARDS_REDUCE: "depositIntoAssetsWallet", //存入平台钱包
   CLAIM_DYNAMIC_EARNINGS_REDUCE: "depositIntoAssetsWallet",
-  CLAIM_SMART_YIELD_WALLET_REDUCE: "depositIntoSmartWallet",
+  CLAIM_SMART_YIELD_WALLET_REDUCE: "withdrawFromSmartWallet",
   TRANSFER: "transaction_TRANSFER",
 };
 
