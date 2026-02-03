5204dfcc2ef15e2250db2936a90e072daf9912bd
abc <abc@example.com>
2026-02-03 16:06:07 +0800

    fee

--- src/views/fund/list.tsx
+++ src/views/fund/list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Icon } from "@/components/icon";
 import { useTrans } from "@/hooks/useTrans";
 import { api } from "@/api";
